0aa78bded299a857087cbcb35fe8cac7ca4ffdbe
liguangsong <863925367@qq.com>
2013-12-20 17:05:28 +0800

    改变第二种数据结构函数定义方法

--- data_structure_2/src/SMS.js
+++ data_structure_2/src/SMS.js
@@ -1,4 +1,4 @@
-function notify_sms_received(json_message){
+function notify_sms_received(json_message) {
     native_accessor.receive_message(json_message);
 }
 
@@ -16,8 +16,8 @@ var native_accessor = {
     }
 };
 
-function judge_and_process_received_apply_message(json_message){
-    var temp_message=json_message.messages[0].message;
+function judge_and_process_received_apply_message(json_message) {
+    var temp_message = json_message.messages[0].message;
 
     if (temp_message.substr(0, 2).toUpperCase() == 'BM') {
         sms.is_or_no_signing_up(json_message);
@@ -27,63 +27,64 @@ function judge_and_process_received_apply_message(json_message){
     }
 }
 
-function sms(){
+function sms() {
 
 }
 
-sms.get_message=function(json_message){
-    var temp_message=json_message.messages[0].message.replace(/\s/g, "");
-    var message=temp_message.substr(2);
+sms.get_message = function (json_message) {
+    var temp_message = json_message.messages[0].message.replace(/\s/g, "");
+    var message = temp_message.substr(2);
     return message;
 }
 
-sms.get_phone=function(json_message){
-    var  phone=json_message.messages[0].phone;
+sms.get_phone = function (json_message) {
+    var phone = json_message.messages[0].phone;
     return phone;
 }
 
-sms.sign_up_status=function(){
+sms.sign_up_status = function () {
     return localStorage.getItem("is_signing_up");
 }
 
-sms.is_or_no_signing_up=function(json_message){
-    var is_signing_up=sms.sign_up_status();
-    if(!(is_signing_up =="false"||is_signing_up==""||is_signing_up==null)){
+sms.is_or_no_signing_up = function (json_message) {
+    var is_signing_up = sms.sign_up_status();
+    if (!(is_signing_up == "false" || is_signing_up == "" || is_signing_up == null)) {
         return  sms.sign_up_is_or_no_repeat(json_message);
     }
 //    return  send
 }
 
-sms.sign_up_is_or_no_repeat=function(json_message){
-    var phone=sms.get_phone(json_message);
-    if(!SignUp.sign_ups_is_or_repeat(phone)){
+sms.sign_up_is_or_no_repeat = function (json_message) {
+    var phone = sms.get_phone(json_message);
+    if (!SignUp.sign_ups_is_or_repeat(phone)) {
         return sms.sign_up_success(json_message);
 
     }
 //    return send;
 }
 
-sms.sign_up_success=function(json_message){
+sms.sign_up_success = function (json_message) {
     SignUp.add_sign_up(json_message);
 }
 
-sms.is_or_no_bidding=function(json_message){
-    var is_bidding=Bidding.bidding_status();
-    if(!(is_bidding =="false"||is_bidding==""||is_bidding==null)){
+sms.is_or_no_bidding = function (json_message) {
+    var is_bidding = Bidding.bidding_status();
+    if (!(is_bidding == "false" || is_bidding == "" || is_bidding == null)) {
         return sms.is_or_no_sign_up(json_message);
     }
     //    return send;
 }
 
-sms.is_or_no_sign_up=function(json_message){
-    var phone=sms.get_phone(json_message);
-    if(Bidding.bid_is_or_not_sign_up(phone)){
+sms.is_or_no_sign_up = function (json_message) {
+    var phone = sms.get_phone(json_message);
+    if (Bidding.bid_is_or_not_sign_up(phone)) {
         return sms.is_or_no_bid_repeat(json_message)
     }
 //    return:send;
 }
-sms.is_or_no_bid_repeat=function(json_message){
-    if(!Bidding.is_or_no_bid_repeat(json_message)){
+
+sms.is_or_no_bid_repeat = function (json_message) {
+    if (!Bidding.is_or_no_bid_repeat(json_message)) {
         return Bidding.bid_success(json_message);
     }
 //    return ;
